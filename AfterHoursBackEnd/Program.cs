08efbeefb0e4aa02659aab7498d6d7fba22072a2
Bernard Polynice <bepoly.nice@gmail.com>
2020-07-08 22:12:54 -0400

    Initialize Repository

---
+++ AfterHoursBackEnd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AfterHoursBackEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            var userInput = Console.ReadLine();
+            var isDecimal = decimal.TryParse(userInput, out decimal userInputAsDecimal);
+
+            if (!isDecimal)
+            {
+                Console.WriteLine("Error - incorrect input - Try again");
+                return;
+            }
+            userInputAsDecimal += 0.5m;
+            Console.WriteLine(userInputAsDecimal);
+
+
+        }
+    }
+}
